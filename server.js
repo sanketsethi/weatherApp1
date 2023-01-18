530940fa0b869ab6187c7d36a73b74986e073055
Sanket <sanketsethi@gmail.com>
2023-01-18 15:29:07 +0000

    Node Module changes

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var request = require('request');
 
 const port = process.env.VCAP_APP_PORT || 5000
 
-app.get('/hello', (req, res) => {
-	console.log("how are you!")
-	console.log(req.query.user)
-	res.send("hellooooooooooo"+' '+req.query.user)
+ app.get('/hello', (req, res) => {
+ 	console.log("how are you!")
+ 	console.log(req.query.user)
+ 	res.send("hellooooooooooo"+' '+req.query.user)
 })
 
-app.get('/weather', (req, res)=>{
+app.get('/weatherApp1', (req, res) => {
+//	console.log("Weather App started!")
 	var options = {
 	  'method': 'GET',
 	  'url': 'https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=b3f703389584f28b8e5874b0bd633fab&units=metric',
@@ -18,15 +19,18 @@ app.get('/weather', (req, res)=>{
 	  }
 	};
 
+	app.listen(port, () => {
+		console.log('Server is running on port 5000')
+	})
+
 	request(options, function (error, response) {
 	  if (error) throw new Error(error);
+	  console.log('Error is raised here'); 
 	  console.log(response.body);
 	});
 })
 
-app.listen(port, () => {
-	console.log('Server is running on port 5000')
-})
+
 
 console.log("welcome Sanket");
 
